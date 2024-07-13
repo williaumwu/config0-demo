9648c1195fa8d6f8e14c240ab0c2be072e8bb725
gary <gary>
2024-07-13 17:23:31 +0800

    'testing platform stuff'

--- execgroups/_config0_configs/ec2_server/_actions/30-exec_terraform.py
+++
@@ -1,9 +0,0 @@
-def default():
-
-    task = {'method': 'shelloutconfig',
-            'metadata': {'env_vars': [],
-                         'shelloutconfigs': ['config0-publish:::terraform::resource_wrapper']
-                         }
-            }
-
-    return task
