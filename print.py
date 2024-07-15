ebd5717b3f4d49159f02214456cc6006373094d3
gary <gary>
2024-07-15 23:19:56 +0800

    'update'

---
+++ print.py
@@ -0,0 +1,51 @@
+"""
+watch out for blank lines
+watch out for using the word space at end of line
+Please be careful which window this types into
+"""
+
+import pyautogui
+import time
+
+spaces = 0
+delay_speed = 0.1 #Bigger number slower typing
+file_path = "/home/gary.leong/Dropbox/ElasticDev/Accounts/williamwu/config0-demo/stacks/_config0_configs/platform-config0-intro/_main/run.py"
+
+def tabbing_mech(line): #Needed for IDEs
+    global spaces
+    count_space = 0
+    for char in line:
+        if char == " ":
+            count_space += 1
+        else:
+            break
+    print(count_space, spaces, line)
+
+    if spaces > count_space:
+        back_tab = (spaces - count_space) // 4 #Number of spaces per indent
+        spaces = count_space
+        print("tab back")
+        for i in range(back_tab):
+            pyautogui.keyDown('shift')
+            pyautogui.press('tab')
+            pyautogui.keyUp('shift')
+        return line.replace("  ", "")
+    elif count_space == 0:
+        return line
+    elif spaces == count_space:
+        return line.replace("  ", "")
+    elif spaces < count_space:
+        print("indenting")
+        spaces = count_space
+        return line.replace("  ", "")
+
+
+for i in range(5): #Seconds before it starts typing
+    time.sleep(1)
+    print(i)
+
+with open(file_path, 'r') as f:
+    for lines in f:
+        #type_me = lines #Use this if on a word processor
+        type_me = tabbing_mech(lines) #Used for IDEs
+        pyautogui.typewrite(type_me, delay_speed)
