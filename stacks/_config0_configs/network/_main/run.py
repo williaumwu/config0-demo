ee5005f89add6e0a6303d293e9f009b350149a51
gary <gary>
2024-08-01 13:01:23 +0800

    'reverted network bac'

--- stacks/_config0_configs/network/_main/run.py
+++ stacks/_config0_configs/network/_main/run.py
@@ -187,8 +187,7 @@ class Main(newSchedStack):
         sched.automation_phase = "infrastructure"
         sched.human_description = "Creates vpc"
         sched.conditions.retries = 1
-        # testtest456
-        sched.on_success = ["network_vars_set","nat_instance"]
+        sched.on_success = ["network_vars_set"]
         self.add_schedule()
 
         sched = self.new_schedule()
@@ -197,7 +196,7 @@ class Main(newSchedStack):
         sched.archive.timewait = 120
         sched.automation_phase = "infrastructure"
         sched.human_description = "Creates variable set"
-        #sched.on_success = ["nat_instance"]
+        sched.on_success = ["nat_instance"]
         self.add_schedule()
 
         sched = self.new_schedule()
